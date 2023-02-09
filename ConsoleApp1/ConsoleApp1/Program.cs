44e40e088662163be958a9fa570eb8675d2d35f7
chrlkru <filimonovdima324@gmail.com>
2023-02-09 11:54:24 +0300

    9 строка

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,3 +6,4 @@ var key = int.Parse(Console.ReadLine()!);
 Console.Write("Укажите путь к новому файлу: ");
 path = Console.ReadLine();
 var writer = new StreamWriter(path!);
+writer.Write(new string(text.Select(e => (char)(e ^ key)).ToArray()));
