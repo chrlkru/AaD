88659599a0df305fe9e6105442c8a3f8384adcc5
TheYOshkinKot <daniilpavlov26@gmail.com>
2023-02-09 11:49:46 +0300

    Строка 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,3 +3,4 @@ var path = Console.ReadLine()!;
 var text = File.ReadAllText(path);
 Console.Write("Укажите ключ: ");
 var key = int.Parse(Console.ReadLine()!);
+Console.Write("Укажите путь к новому файлу: ");
